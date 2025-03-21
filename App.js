71c30c4f5d38d3813612b811f9599e04f310bcd8
Aditya Pimpale <pimpaleaditya2@gmail.com>
2025-03-21 23:03:45 +0530

    #2 added custom reusable button in atoms, so it can be used anywhere as per need
    installed react native paper for Good UI

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navbar from './src/components/navbar'; 
+import CustomButton from './src/atoms/CustomButton.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Navbar /> 
+      <CustomButton 
+        style={{ width: 150, height: 50 }} 
+        title="Click Me"
+        useTouchableOpacity={true} 
+        mode="contained"
+        onPress={() => console.log('Button Pressed')}
+      />
       <Text style={{ fontSize: 50 }}>Prep Nation</Text>
       <StatusBar style="auto" />
     </View>
